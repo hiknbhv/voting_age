d0ac2901be68a8c49d25ab4ee08a8017bc3ee249
Tamilea pimpton <pimptam0000@warrren.k12.in.us>
2024-10-25 09:12:20 -0400

    voting

---
+++ main.py
@@ -0,0 +1,11 @@
+age = int(input("Please enter your age:"))
+
+is_eligible = age < 17
+if age >=18:
+    print("you are eligible to vote")
+else:
+    print("your still a kid")
+    print ("you can NOT vote")
+
+print(" ")
+print("Program Completed")
